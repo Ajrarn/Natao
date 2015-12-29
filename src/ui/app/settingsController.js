88e742fa9a52b1109e4e670e03b13e6a95cdc1cc
Christophe Dos Santos <christophe.ajrarn@gmail.com>
2015-12-29 23:24:08 +1100

    feat(init) : choose language

--- src/ui/app/settingsController.js
+++ src/ui/app/settingsController.js
@@ -6,7 +6,7 @@
         .controller('SettingsController', SettingsController);
 
 
-    function SettingsController(PreferencesService,DatabaseService,$location,$translate,$sce,fileDialog) {
+    function SettingsController($rootScope,PreferencesService,DatabaseService,$location,$translate,$sce,fileDialog) {
         console.log('SettingsController');
 
         var self = this;
@@ -18,6 +18,19 @@
         self.firstTime = self.PreferencesService.fileExist();
         self.fileDialog = fileDialog;
 
+        // for the first time, we have to go step by step
+        if (!self.PreferencesService.fileExist()) {
+            self.step = 1;
+        } else {
+            self.step = 5;
+        }
+
+        // The only way to the good message with asynchroneous loading of translation
+        $rootScope.$on('$translateChangeSuccess', function () {
+            self.message = $translate.instant('SETTING_MESSAGE');
+        });
+
+
         self.settingsValide = function() {
             self.valid = self.PreferencesService.isValid();
         };
@@ -27,17 +40,14 @@
             $location.path( '/app' );
         };
 
-        self.changeLanguage = function () {
+        self.changeLanguage = function(language) {
+            self.PreferencesService.preferences.language = language;
             $translate.use(self.PreferencesService.preferences.language);
-            self.initMessage();
+            //self.initMessage();
             self.settingsValide();
+            self.step = 2;
         };
 
-        self.initMessage = function() {
-            $translate('SETTING_MESSAGE').then(function (translation) {
-                self.message = self.$sce.trustAsHtml(translation);
-            });
-        };
 
         self.newDatabase = function() {
             self.fileDialog.saveAs(function(filename) {
@@ -49,10 +59,27 @@
         self.chooseDatabase = function() {
             self.fileDialog.openFile(function(filename){
                 self.PreferencesService.settings.fileDatabase = filename;
+                self.PreferencesService.init();
             }, false, ['db']);
+
         };
 
-        self.initMessage();
+        self.showMessage = function() {
+            return (self.step > 1)
+        };
+
+        self.showFile = function() {
+            return (self.step > 1)
+        };
+
+        self.showId = function() {
+            return (self.step > 2)
+        };
+
+        self.showColor = function() {
+            return (self.step > 3)
+        };
+
         self.settingsValide();
 
 
