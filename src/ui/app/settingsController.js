8fe3cac031399ab36f62999ed6a1db6f5c6435db
Christophe Dos Santos <christophe.ajrarn@gmail.com>
2016-02-27 14:20:25 +1100

    feat(settings): settings with tabs for style and templates

--- src/ui/app/settingsController.js
+++ src/ui/app/settingsController.js
@@ -6,7 +6,7 @@
         .controller('SettingsController', SettingsController);
 
 
-    function SettingsController($rootScope,$scope,PreferencesService,DatabaseService,$location,$translate,$sce,fileDialog) {
+    function SettingsController($rootScope,$scope,PreferencesService,DatabaseService,$location,$sce,fileDialog) {
         console.log('SettingsController');
 
         var self = this;
@@ -17,19 +17,6 @@
         self.$scope = $scope;
 
         self.fileDialog = fileDialog;
-
-        // for the first time, we have to go step by step
-        if (!self.PreferencesService.fileSettingsExist()) {
-            self.step = 1;
-        } else {
-            self.step = 5;
-        }
-
-        // The only way to the good message with asynchroneous loading of translation
-        $rootScope.$on('$translateChangeSuccess', function () {
-            self.message = $translate.instant('SETTING_MESSAGE');
-        });
-
 
         self.settingsValide = function() {
             self.valid = self.PreferencesService.isValid();
@@ -72,25 +59,6 @@
 
         };
 
-        self.showMessage = function() {
-            return (self.step > 1)
-        };
-
-        self.showFile = function() {
-            return (self.step > 1)
-        };
-
-        self.showId = function() {
-            return (self.step > 2)
-        };
-
-        self.showColor = function() {
-            return (self.step > 3)
-        };
-
-        self.showDone = function() {
-            return (self.step > 3 && self.valid)
-        };
 
         self.settingsValide();
 
