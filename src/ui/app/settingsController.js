fd7ca197cb9c846da07dc64fcc8f65a87ebba7c4
Christophe Dos Santos <christophe.ajrarn@gmail.com>
2016-03-01 18:43:57 +1100

    feat(beautify): add beautify to read pretty html

--- src/ui/app/settingsController.js
+++ src/ui/app/settingsController.js
@@ -1,6 +1,8 @@
 (function () {
     "use strict";
 
+    var beautify_html = require('js-beautify').html;
+
     angular
         .module('Natao')
         .controller('SettingsController', SettingsController);
@@ -76,11 +78,41 @@
         };
 
         self.changeDocument = function() {
-          self.currentHTML = self.$showdown.makeHtml(self.currentDoc.md);
+            self.currentHTML = self.$showdown.makeHtml(self.currentDoc.md);
+            self.allHtml();
         };
 
         self.changeCss = function() {
             self.CssService.initCurrentByContent(self.currentCss.css);
+        };
+
+        self.editorReadOnly = function(_editor) {
+            // Options
+            _editor.setReadOnly(true);
+        };
+
+        self.allHtml = function() {
+
+            var completeHtml = '<div class="haut" layout="row" layout-align="start stretch">' +
+                '<div class="identity" layout="column" layout-align="center center">' +
+                '<p>' + self.PreferencesService.preferences.name +'</p>' +
+                '<p>' + self.PreferencesService.preferences.firstName +'</p>' +
+                '</div>' +
+                '<div class="title-zone" layout="column" layout-align="center center">' +
+                '<h1>' + self.currentDoc.title +'</h1>' +
+                '<p>' + self.currentDoc.created + '</p>' +
+                '</div>' +
+                '</div>' +
+                '<div class="notation"></div>' +
+                '<div class="devoir" flex layout="row" layout-align="start stretch">' +
+                '<div class="marge"></div>' +
+                '<div id="rendu" flex>'+ self.currentHTML +'</div>' +
+                '</div>';
+
+            console.log(self.currentHTML);
+            console.log(completeHtml);
+
+            self.currentHtmlAll = beautify_html(completeHtml);
         };
 
 
