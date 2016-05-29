6e52d90dc98e100e690db64f573a8d26389077f3
Christophe Dos Santos <christophe.ajrarn@gmail.com>
2016-05-30 07:25:11 +1100

    fix(translate): init when translate is ready

--- app/src/ui/app/loadingController.js
+++ app/src/ui/app/loadingController.js
@@ -7,7 +7,7 @@
         .controller('LoadingController', LoadingController);
 
 
-    function LoadingController(PreferencesService,CssService,TemplateTreeService,PrincipalTreeService,$location,OnBoardingService,$q) {
+    function LoadingController(PreferencesService,CssService,TemplateTreeService,PrincipalTreeService,$location,OnBoardingService,$q,$translate) {
 
 
         this.PreferencesService = PreferencesService; //The preferences init send us here
@@ -17,21 +17,27 @@
         this.PrincipalTreeService = PrincipalTreeService;
         this.OnBoardingService = OnBoardingService
         this.$location = $location;
+        this.$translate = $translate;
 
         var self = this;
 
         //when all the services are ready we go to the editor
-        self.CssService.getInitCss().then(function(defaultCss) {
-            var templatePromise = self.TemplateTreeService.getInitTemplate();
-            var principalTreePromise = self.PrincipalTreeService.getInitTreeService(defaultCss);
-            var onBoardingPromise = OnBoardingService.init();
 
-            $q.all([templatePromise,principalTreePromise,onBoardingPromise])
-                .then(function() {
-                    $location.path('/editor');
+        $translate.onReady()
+            .then(function() {
+                self.CssService.getInitCss().then(function(defaultCss) {
+                    var templatePromise = self.TemplateTreeService.getInitTemplate();
+                    var principalTreePromise = self.PrincipalTreeService.getInitTreeService(defaultCss);
+                    var onBoardingPromise = OnBoardingService.init();
+
+                    $q.all([templatePromise,principalTreePromise,onBoardingPromise])
+                        .then(function() {
+                            $location.path('/editor');
+                        });
+
                 });
-            
-        });
+            });
+       
     }
 
 }());
