68552370f288845b31b81c2b182e206d2e4f4f8f
Ajrarn <christophe.ajrarn@gmail.com>
2020-04-02 14:47:03 +1100

    feat(route): add the first route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConfigComponent } from './config/config.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'config',
+    component: ConfigComponent
+  },
+  {
+    path:  '',
+    redirectTo: 'config',
+    pathMatch:  'full'
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
